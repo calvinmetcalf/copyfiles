aee6f9e1a74d011d270143ea53bfb2dfe13a52fe
aelbore <aelbore.sg@gmail.com>
2019-05-31 21:11:06 +0800

    change const and let to var to support nodejs 4

--- mkdirp.js
+++ mkdirp.js
@@ -1,10 +1,10 @@
-const path = require('path')
-const fs = require('fs')
+var path = require('path')
+var fs = require('fs')
 
 function mkdirp(directory) {
-  const dirPath = path.resolve(directory).replace(/\/$/, '').split(path.sep);
-  for (let i = 1; i <= dirPath.length; i++) {
-    const segment = dirPath.slice(0, i).join(path.sep);
+  var dirPath = path.resolve(directory).replace(/\/$/, '').split(path.sep);
+  for (var i = 1; i <= dirPath.length; i++) {
+    var segment = dirPath.slice(0, i).join(path.sep);
     if (!fs.existsSync(segment) && segment.length > 0) {
       fs.mkdirSync(segment);
     }
