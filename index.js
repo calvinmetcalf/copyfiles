e003d778379e12cb5f8db06cb36c81a68f079d17
Jay <aelbore.sg@gmail.com>
2019-05-31 15:34:33 +0800

    remove mkdirp dependencies
    remove mkdirp dependencies

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 var path = require('path');
 var fs = require('fs');
 var glob = require('glob');
-var mkdirp = require('mkdirp');
 var through = require('through2').obj;
 var noms = require('noms').obj;
+
+function mkdirp(directory: string): void {
+  const dirPath = path.resolve(directory).replace(/\/$/, '').split(path.sep);
+  for (let i = 1; i <= dirPath.length; i++) {
+    const segment = dirPath.slice(0, i).join(path.sep);
+    if (!fs.existsSync(segment) && segment.length > 0) {
+      fs.mkdirSync(segment);
+    }
+  }
+}
+
 function toStream(array) {
   var length = array.length;
   var i = 0;
